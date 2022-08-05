168848f75ffea4c4e2034d153816275198a51cd7
dfakinloye <108558440+dfakinloye@users.noreply.github.com>
2022-08-05 14:13:01 -0400

    Create common.groovy

---
+++ common.groovy
@@ -0,0 +1,12 @@
+def call(string stageName){
+  if("${stageName}" == "Build"){
+    sh "mvn clean package"
+  }
+  else if ("${stageName}" == "SonarQube Report"){
+    sh "mvn sonar:sonar"
+  }
+  else if ("${stageName}" == "Upload Into Nexus"){
+    sh "mvn deploy"
+  }
+}
+    
